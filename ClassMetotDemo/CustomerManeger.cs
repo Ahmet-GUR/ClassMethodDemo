40809ed17cc9739570a56db48cbbecc153a6b9be
A.G <A.G@ZENOPANELLTDT>
2021-01-19 09:11:08 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManeger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManeger
+    {
+        public void GetList(Customer customer)
+        {
+            Console.WriteLine( customer.Id + " " + customer.FirstName + " " + customer.LastName + " " + customer.Age);
+        }
+
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer Added!" +" = "+ customer.Id + " " + customer.FirstName + " " + customer.LastName + " " + customer.Age);
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer Delete!" + " = " + customer.Id + " " + customer.FirstName + " " + customer.LastName + " " + customer.Age);
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Customer Update!" + " = " + customer.Id + " " + customer.FirstName + " " + customer.LastName + " " + customer.Age);
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "Ahmet";
+            customer1.LastName = "GÜR";
+            customer1.Age = 16;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.FirstName = "Hasan";
+            customer2.LastName = "ALTUNDAĞ";
+            customer2.Age = 25;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Pelin";
+            customer3.LastName = "YILMAZ";
+            customer3.Age = 20;
+
+            Customer customer4 = new Customer();
+            customer4.Id = 4;
+            customer4.FirstName = "Merve";
+            customer4.LastName = "ASLANOBA";
+            customer4.Age = 35;
+
+            Customer customer5 = new Customer();
+            customer5.Id = 5;
+            customer5.FirstName = "Hakan";
+            customer5.LastName = "KALDIRIM";
+            customer5.Age = 30;
+
+            CustomerManeger customerManeger = new CustomerManeger();
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4, customer5 };
+
+            foreach (var customer in customers)
+            {
+                customerManeger.GetList(customer);
+            }
+
+            Line();
+
+            customerManeger.Add(customer1);
+
+            Line();
+
+            customerManeger.Delete(customer2);
+
+            Line();
+
+            customerManeger.Update(customer3);
+
+            Console.ReadLine();
+        }
+
+        private static void Line()
+        {
+            Console.WriteLine("---------------------");
+        }
+    }
+}
